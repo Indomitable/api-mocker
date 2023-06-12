ff97fe1658760889792a6f578cd44d25d8a8afde
Ventsislav Mladenov <v.mladenov@hotmail.com>
2023-06-12 14:56:09 +0200

    feature: add config default values
    Signed-off-by: Ventsislav Mladenov <v.mladenov@hotmail.com>

--- ApiMocker/ApiMocker/Models/RequestMock.cs
+++ ApiMocker/ApiMocker/Models/RequestMock.cs
@@ -2,9 +2,9 @@ namespace ApiMocker.Models;
 
 public sealed class RequestMock
 {
-    public string Path { get; set; }
-    public string Method { get; set; }
-    public int StatusCode { get; set; }
+    public string Path { get; set; } = "/";
+    public string Method { get; set; } = "GET";
+    public int StatusCode { get; set; } = StatusCodes.Status200OK;
     public Dictionary<string, string> Headers { get; set; } = new ();
-    public string Body { get; set; }
+    public string? Body { get; set; }
 }
--- ApiMocker/ApiMocker/Models/Server.cs
+++ ApiMocker/ApiMocker/Models/Server.cs
@@ -2,9 +2,9 @@ namespace ApiMocker.Models;
 
 public sealed class Server
 {
-    public string Url { get; set; }
+    public string Url { get; set; } = "http://localhost:5000";
 
     public Dictionary<string, string> Headers { get; set; } = new();
 
-    public List<RequestMock> Mocks { get; set; }
+    public List<RequestMock> Mocks { get; set; } = new();
 }
